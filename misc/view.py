3b6fde5c093f6700635798f4fc0b4250dcb8c275
cuzhucuncheng <cuzhu@ucsd.edu>
2022-06-20 15:08:12 -0700

    refactor: rm imp dependency

--- misc/view.py
+++ misc/view.py
@@ -1,21 +1,21 @@
 import pymem3dg.visual as dg_vis
 import pymem3dg.read as dg_read
 import matplotlib.pyplot as plt
-import imp
 import numpy as np
 import polyscope as ps
 import pymem3dg as dg
-
+import sys
 ####################################################
 #                 Initialize pathes                #
 ####################################################
 # folder = r"H:/Shared drives/Rangamani Lab Drive/Cuncheng Zhu/manuscript_in_process/feng_pearling/repo/aggregation_pearl/chi20"
-folder = "/Users/cuncheng/Dev/aggregation_pearl/run8"
+folder = "../run8"
 var = 100
 subFolder = folder + f"/chi{var}"
-parameterFile = imp.load_source("module.name", folder + "/parameters.py")
-xi, A_bar, R_bar, Kb, h = parameterFile.scalingVariables()
-parameters = parameterFile.parameters(xi, A_bar, R_bar, Kb)
+sys.path.append(folder)
+import parameters as ps
+xi, A_bar, R_bar, Kb, h = ps.scalingVariables()
+parameters = ps.parameters(xi, A_bar, R_bar, Kb)
 parameters.aggregation.chi = var * Kb / R_bar**2
 trajNc = subFolder + "/traj.nc"
 ####################################################
@@ -29,100 +29,99 @@ trajNc = subFolder + "/traj.nc"
 # ps_mesh = ps.register_surface_mesh("mesh", vertex, face, smooth_shade=True)
 # ps.show()
 
-dg_vis.animate(
-    trajNc,
-    frames=[dg_read.sizeOf(trajNc) - 1],
-    parameters=parameters,
-)
-ps.screenshot("hello.png")
+# dg_vis.animate(
+#     trajNc,
+#     frames=[dg_read.sizeOf(trajNc) - 1],
+# )
+# ps.screenshot("hello.png")
 ####################################################
 #            visualize animated .nc                #
 ####################################################
-dg_vis.animate(
-    trajNc,
-    frames=[5],
-    parameters=parameters,
-    meanCurvature=True,
-    gaussianCurvature=True,
-    bendingForce=True,
-    externalForce=True,
-    mechanicalForce=True,
-    capillaryForce=True,
-    lineCapillaryForce=True,
-    osmoticForce=True,
-    springForce=True,
-    entropyForce=True,
-    chemicalPotential=True,
-    bendingPotential=True,
-    diffusionPotential=True,
-    aggregationPotential=True,
-    adsorptionPotential=True,
-    entropyPotential=True,
-    inPlaneFluxForm=True,
-)
+# dg_vis.animate(
+#     trajNc,
+#     frames=[5],
+#     parameters=parameters,
+#     meanCurvature=True,
+#     gaussianCurvature=True,
+#     bendingForce=True,
+#     externalForce=True,
+#     mechanicalForce=True,
+#     capillaryForce=True,
+#     lineCapillaryForce=True,
+#     osmoticForce=True,
+#     springForce=True,
+#     entropyForce=True,
+#     chemicalPotential=True,
+#     bendingPotential=True,
+#     diffusionPotential=True,
+#     aggregationPotential=True,
+#     adsorptionPotential=True,
+#     entropyPotential=True,
+#     inPlaneFluxForm=True,
+# )
 # ps.show()
 ####################################################
 #                         plots                    #
 ####################################################
-# sp_size = (3,2)
-# fig, axs = plt.subplots(sp_size[0], sp_size[1])
-# dg_vis.matplotlibStyle(9, 10, 12)
-# fig.set_size_inches(8, 9)
-# count = 0
-# dg_vis.plotEnergy(
-#     axs[np.unravel_index(count, sp_size, "F")],
-#     trajNc,
-#     parameters,
-#     # logScale=True,
-#     zeroing=True,
-#     potentialEnergy=True,
-#     # kineticEnergy=True,
-#     # totalEnergy=True,
-#     bendingEnergy=True,
-#     # externalWork=True,
-#     # deviatoricEnergy=True,
-#     # surfaceEnergy=True,
-#     # pressureEnergy=True,
-#     # adsorptionEnergy=True,
-#     aggregationEnergy=True,
-#     entropyEnergy=True,
-#     # edgeSpringEnergy=True,
-#     # faceSpringEnergy=True,
-#     # lcrSpringEnergy=True,
-#     dirichletEnergy=True,
-# )
-# count = count + 1
+sp_size = (3,2)
+fig, axs = plt.subplots(sp_size[0], sp_size[1])
+dg_vis.matplotlibStyle(9, 10, 12)
+fig.set_size_inches(8, 9)
+count = 0
+dg_vis.plotEnergy(
+    axs[np.unravel_index(count, sp_size, "F")],
+    trajNc,
+    parameters,
+    # logScale=True,
+    zeroing=True,
+    potentialEnergy=True,
+    # kineticEnergy=True,
+    # totalEnergy=True,
+    bendingEnergy=True,
+    # externalWork=True,
+    # deviatoricEnergy=True,
+    # surfaceEnergy=True,
+    # pressureEnergy=True,
+    # adsorptionEnergy=True,
+    aggregationEnergy=True,
+    entropyEnergy=True,
+    # edgeSpringEnergy=True,
+    # faceSpringEnergy=True,
+    # lcrSpringEnergy=True,
+    dirichletEnergy=True,
+)
+count = count + 1
 
-# dg_vis.plotChemicalPotentials(
-#     axs[np.unravel_index(count, sp_size, "F")],
-#     trajNc,
-#     parameters,
-#     logScale=True,
-#     bendingPotential=True,
-#     # deviatoricPotential=True,
-#     aggregationPotential=True,
-#     entropyPotential=True,
-#     dirichletPotential=True,
-#     # adsorptionPotential=True,
-# )
-# count = count + 1
+dg_vis.plotChemicalPotentials(
+    axs[np.unravel_index(count, sp_size, "F")],
+    trajNc,
+    parameters,
+    logScale=True,
+    bendingPotential=True,
+    # deviatoricPotential=True,
+    aggregationPotential=True,
+    entropyPotential=True,
+    dirichletPotential=True,
+    # adsorptionPotential=True,
+)
+count = count + 1
 
-# dg_vis.plotMechanicalForces(
-#     axs[np.unravel_index(count, sp_size, "F")],
-#     trajNc,
-#     parameters,
-#     logScale=True,
-#     bendingForce=True,
-#     # capillaryForce=True,
-#     # osmoticForce=True,
-#     # adsorptionForce=True,
-#     lineCapillaryForce=True,
-#     aggregationForce=True,
-#     # externalForce=True,
-#     entropyForce=True,
-#     # springForce=True,
-# )
-# count = count + 1
+dg_vis.plotMechanicalForces(
+    axs[np.unravel_index(count, sp_size, "F")],
+    trajNc,
+    parameters,
+    logScale=True,
+    bendingForce=True,
+    # capillaryForce=True,
+    # osmoticForce=True,
+    # adsorptionForce=True,
+    lineCapillaryForce=True,
+    aggregationForce=True,
+    # externalForce=True,
+    entropyForce=True,
+    # springForce=True,
+)
+count = count + 1
 
 # count = np.prod(sp_size) - 1
 # dg_vis.overlayColorMap(
@@ -133,5 +132,6 @@ dg_vis.animate(
 #     orientation="vertical",
 # )
 
-# fig.tight_layout()
+fig.tight_layout()
 # plt.show()
+plt.savefig("hello.png")
