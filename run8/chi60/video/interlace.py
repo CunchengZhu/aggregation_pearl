61b818f2826e3c6aac11b289269f271e9e3d22a1
Cuncheng Zhu <cuzhu@ucsd.edu>
2022-06-21 22:32:42 -0700

    chore: add conveience scripts for video generation in subfolders

---
+++ run8/chi60/video/interlace.py
@@ -0,0 +1,16 @@
+import moviepy.editor as mpy
+import numpy as np
+files = []
+frameList = np.tile(np.arange(0, 120, 1), 3)
+for frame in frameList:
+    frame = int(frame)
+    png = "recordings/frame{}.png".format(frame)
+    # matplotlibStyle()
+    # fig, ax = plt.subplots(1, 1)
+    # fig.set_size_inches(4, 3)
+    # overlayColorMap(ax, png, [0, 1], "$\phi$", orientation="vertical")
+    # figureName = "colormapped/frame{}.png".format(frame)
+    # fig.savefig(figureName, transparent=True, dpi=1500)
+    files.append(png)
+clip = mpy.ImageSequenceClip(files, fps = 20)
+clip.write_gif("test.gif")
